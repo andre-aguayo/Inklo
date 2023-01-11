b6fab961614c63e46809f5c93d176c894c11e0e0
Andre Aguayo <andre.aguayo@hotmail.com>
2023-01-11 03:16:02 -0300

    Changes into api route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\User\UserController;
-use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,38 +14,8 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-Route::post('isconnected', function () {
-    return response()->json(['success' => true, 'isconnectes' => true]);
-});
+Route::get('isconnected', function () {
+    return response()->json(['isconnectes' => true]);
+})->middleware(['api.response']);
 
-Route::post('login', [LoginController::class, 'login'])
-    ->middleware('api.response');
-
-/**
- * Routes for admin access
- */
-Route::middleware(['auth.admin', 'auth:sanctum', 'api.response'])
-    ->prefix('admin')
-    ->name('admin')
-    ->group(function () {
-        Route::get('isconnected', function () {
-            return response(['isconnected' => true]);
-        });
-    });
-
-/**
- * Routes for user access
- */
-Route::middleware(['api.response', 'auth:sanctum'])
-    ->prefix('user')
-    ->name('admin')
-    ->group(function () {
-
-        Route::get('isconnected', function () {
-            return response(['isconnectes' => true]);
-        });
-    });
-
-Route::post('/send-mail', [CompanyController::class, 'sendMail']);
-
-Route::get('/home', [CompanyController::class, 'index']);
+Route::post('salvar-local', [UserController::class, 'store'])->middleware(['api.response'])->name('user.store');
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        //List all Companies route
+        // ($this->get('/api/companies'))
+        //     ->assertStatus(200)
+        //     ->assertJson(['success' => true]);
+    }
+}
